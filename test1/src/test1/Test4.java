a6759b53a71822a6dba0e35ff6593427fb40d349
hkl <hkl@192.168.198.1>
2020-02-09 18:48:11 +0800

    1

---
+++ test1/src/test1/Test4.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Test4 {
+	public static void main(String[] args) {
+		Transition t = new Transition();
+		int a = 100;
+		int b = 1;
+		System.out.println(t.m(a)==b);
+	}
+}
---
+++ test1/src/test1/Transition.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Transition {
+
+	private Integer num;
+	public static int cm(Integer num) {
+		 num = num * 100;
+		return num;
+	}
+	public static int m(Integer num) {
+		num = num / 100;
+		return num;
+	}
+}
